dd6ca5f40e669eeb2df6af125833f76a4fd63fd6
elvis2121 <elvisnjau@gmail.com>
2018-08-17 17:03:45 +0300

    config file setup

---
+++ apiv1/instance/config.py
@@ -0,0 +1,28 @@
+"""configuration file for the questions app"""
+
+
+class Config():
+    """The base class for configs"""
+    DEBUG = True
+    CSRF_ENABLED = True
+
+
+class DevelopmentConfig(Config):
+    """Defines the development environment configs"""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Defines the testing environment configs"""
+
+    TESTING = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Defines the production environment configs"""
+    DEBUG = False
+    TESTING = False
+
+
+APP_CONFIG = {'development': DevelopmentConfig, 'testing': TestingConfig,
+              'production': ProductionConfig}
