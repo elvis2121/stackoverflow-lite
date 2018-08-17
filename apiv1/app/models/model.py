bca08d7feacaf2fb63512c47f24ba59ecb3fd255
elvis2121 <elvisnjau@gmail.com>
2018-08-17 17:37:07 +0300

    question model

---
+++ apiv1/app/models/model.py
@@ -0,0 +1,18 @@
+"""The model definition"""
+
+questions = [
+    {
+        'id': 1,
+        'title': u'restful APIs',
+        'description': u'how do i build an API with flaskrestful extension?',
+        'answered': False,
+        'myanswer': ''
+    },
+    {
+        'id': 2,
+        'title': u'mobile apps',
+        'description': u'how do i build mobile apps for android applications?',
+        'answered': False,
+        'myanswer': ''
+    }
+]
